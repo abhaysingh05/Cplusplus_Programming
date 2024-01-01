3c1c471256811e1d0f35ed2c2996d2fa71819e2a
abhaysingh05 <abhaysingh0948@gmail.com>
2024-01-01 19:07:18 +0530

    Testing

---
+++ A_Transformation_from_A_to_B.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <vector>
+using namespace std;
+
+void run_case() 
+{
+    
+    int64_t a,b;
+    cin >> a >> b;
+    dfs(a);
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tests = 1;
+    cin >> tests;
+
+    while (tests-- > 0)
+        run_case();
+}
