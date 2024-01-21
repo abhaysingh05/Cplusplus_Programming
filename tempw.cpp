4cbd1c2e58a623011f71ef0546c04c5662bc5e99
abhaysingh05 <abhaysingh0948@gmail.com>
2024-01-21 14:45:41 +0530

    Excel sheet

--- tempw.cpp
+++ tempw.cpp
@@ -1,36 +1,99 @@
-#include <algorithm>
-#include <array>
-#include <bitset>
-#include <cassert>
-#include <chrono>
-#include <cmath>
-#include <cstdint>
-#include <cstring>
-#include <functional>
-#include <iomanip>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <random>
-#include <set>
-#include <vector>
+//{ Driver Code Starts
+#include <bits/stdc++.h>
 using namespace std;
 
-void run_case() 
-{
-    
-    //Your code here
+// } Driver Code Ends
 
-}
+class Solution1
+{
+    public:
+        int findNum(int n)
+        {
+          int low=0;
+          int high=5*n; //(x/5<=n)
+          int ans=low;
+          int cnt;
+          while(low<=high)
+          {
+              int mid=(low+high)/2;
+              cnt=count_zero(mid);
+              if(cnt>=n)
+              {
+                  ans=mid;
+                  high=mid-1;
+              }
+              else
+              low=mid+1;
+          }
+        }
+        
+        int count_zero(int x)
+        {
+            int deno=5;
+            int count=0;
+            while(x>=deno)
+            {
+                count+=(x/deno);
+                deno*=5;
+            }
+            return count;
+        }
+};
+class Solution
+{
+    public:
+        int count(int x)
+        {
+            int temp = 5;
+            int val = 0;
+            while(temp <= x)
+            {
+                val += x/temp;
+                temp *= 5;
+            }
+            return val;
+        }
+        int findNum(int n)
+        {
+            int l = 0;
+            int r = 5*n;
+            int mid;
+            // f f f t t t t
+            while(r - l > 1)
+            {
+                mid  = (l+r)/2;
+                if(count(mid) >= n)
+                {
+                    r = mid;
+                }
+                else
+                {
+                    l = mid + 1;
+                }
+            }
+            if(count(l) >= n)
+            {
+                return l;
+            }
+            else
+            {
+                return r;
+            }
+        }
+};
+
+//{ Driver Code Starts.
+
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int tests = 1;
-    cin >> tests;
-
-    while (tests-- > 0)
-        run_case();
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        Solution ob;
+        cout <<ob.findNum(n) << endl;
+    }
+return 0;
 }
+// } Driver Code Ends
