3879e84b6ef85e1b01cb9b86c3daf7c64c9cdb2d
abhay-singh <abhaysingh0948@gmail.com>
2024-01-21 16:28:56 +0530

    Changes

--- tempu.cpp
+++ tempu.cpp
@@ -17,76 +17,11 @@
 #include <vector>
 using namespace std;
 
-void run_case(int64_t &tttt) 
+void run_case(int64_t &tttt)
 {
     // cout << "#Case " << tttt << ": ";
-    int n;
-    cin >> n;
-    vector<int> a(n+1);
-    map<int,int> val;
-    for (int i = 1; i <= n; ++i)
-    {
-        cin >> a[i];
-    }
-    vector<int> v2(n+1);
-    vector<int> v1(n+1);
-    val[1] = 1;
-    val[n] = -1;
-    for (int i = 2; i < n; ++i)
-    {
-        int64_t l = abs(a[i]-a[i-1]);
-        int64_t r = abs(a[i]-a[i+1]);
-        if(l < r)
-        {
-            val[i] = -1;
-        }
-        else
-        {
-            val[i] = 1;
-        }
-    }
-    v1[1] = 0;
-    v2[n] = 0;
-    for (int i = 2; i <= n; ++i)
-    {
-        if(val[i] == -1)
-        {
-            v1[i] = 1 + v1[i-1];
-        }
-        else
-        {
-            v1[i] = abs(a[i]-a[i-1]) + v1[i-1];
-        }
-    }
-    for (int i = n-1; i >= 1; --i)
-    {
-        if(val[i] == 1)
-        {
-            v2[i] = 1 + v2[i+1];
-        }
-        else
-        {
-            v2[i] = abs(a[i]-a[i+1]) + v2[i+1];
-        }
-    }
-    int m;
-    cin >> m;
-    while(m--)
-    {
-        int x,y;
-        cin >> x >> y;
-        if(x == y) cout << 0 << "\n";
-        else if(y > x)
-        {
-            cout << v2[x] - v2[y] << "\n";
-        }
-        else
-        {
-            cout << v1[x] - v1[y] << "\n";
-        }
-    }
-    
 
+    //Your code here
 
 }
 
