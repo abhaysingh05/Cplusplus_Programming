a66aa639e48ac8f4903bb7539f9720d67eef50fd
Abhay Singh <abhaysingh0948@gmail.com>
2024-02-24 01:26:22 +0530

    new files

---
+++ B_Sasha_and_the_Drawing.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+// Macros
+#define ff first
+#define ss second
+#define pb push_back
+#define rep(i, a, b) for (int i = (a); i <= (b); ++i)
+#define all(x) x.begin(), x.end()
+
+// Typedef
+typedef long long ll;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+    ordered_set; // .find_by_order(), .order_of_key()
+
+// Constants
+const int INF = (int)1e9;
+const int mod = INF + 7;
+/**************************************************************************************/
+
+void runCase(int &testCase) {
+    // cout << "#Case " << testCase << ": ";
+
+    int n, k;
+    cin >> n >> k;
+    if (k == 4 * n - 2)
+        cout << k / 2 + 1 << "\n";
+    else if (k & 1)
+        cout << k / 2 + 1 << "\n";
+    else
+        cout << k / 2 << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int tests = 1;
+    cin >> tests;
+
+    rep(i, 1, tests) runCase(i);
+
+    return 0;
+}
---
+++ B_Vlad_and_Shapes.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+// Macros
+#define ff first
+#define ss second
+#define pb push_back
+#define rep(i, a, b) for (int i = (a); i <= (b); ++i)
+#define all(x) x.begin(), x.end()
+
+// Typedef
+typedef long long ll;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+    ordered_set; // .find_by_order(), .order_of_key()
+
+// Constants
+const int INF = (int)1e9;
+const int mod = INF + 7;
+/**************************************************************************************/
+
+void runCase(int &testCase) {
+    // cout << "#Case " << testCase << ": ";
+
+    int n;
+    cin >> n;
+    vector<string> v(n);
+    for (auto &e : v) cin >> e;
+    for (auto &e : v)
+        if (count(all(e), '1') == 1) {
+            cout << "TRIANGLE\n";
+            return;
+        }
+    cout << "SQUARE\n";
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int tests = 1;
+    cin >> tests;
+
+    rep(i, 1, tests) runCase(i);
+
+    return 0;
+}
