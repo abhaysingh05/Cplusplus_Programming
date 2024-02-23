83da4b133acf9b103cad1d3531c6dd7d2b94f4a7
Abhay Singh <abhaysingh0948@gmail.com>
2024-02-23 11:23:07 +0530

    temp files

--- tempw.cpp
+++ tempw.cpp
@@ -13,44 +13,26 @@ using namespace __gnu_pbds;
 typedef long long ll;
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
     ordered_set; // .find_by_order(), .order_of_key()
-/**************************************************************************************/
-int cnt = 0, n;
-vector<bool> column;
-vector<bool> diag1;
-vector<bool> diag2;
-void search(int y) {
-    if (y == n) {
-        cnt++;
-        return;
-    }
-    for (int x = 0; x < n; x++) {
-        if (column[x] || diag1[x + y] || diag2[x - y + n - 1]) continue;
-        column[x] = diag1[x + y] = diag2[x - y + n - 1] = true;
-        search(y + 1);
-        column[x] = diag1[x + y] = diag2[x - y + n - 1] = false;
-    }
-}
 
-void run_case(int &testCase) {
+// Constants
+const int INF = (int)1e9;
+const int mod = INF + 7;
+/**************************************************************************************/
+
+void runCase(int &testCase) {
     // cout << "#Case " << testCase << ": ";
 
-    cin >> n;
-    column.resize(n);
-    diag1.resize(2 * n - 1);
-    diag2.resize(2 * n - 1);
-    fill(all(column), false);
-    fill(all(diag1), false);
-    fill(all(diag2), false);
-    search(0);
-    cout << cnt << "\n";
+    
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int64_t tests = 1;
-    // cin >> tests;
+    ll tests = 1;
+    cin >> tests;
 
-    rep(i, 1, tests) run_case(i);
+    rep(i, 1, tests) runCase(i);
+
+    return 0;
 }
