f220afbf36971dd901c5fc8e0dae9ed8ec9f6a68
Abhay Singh <abhaysingh0948@gmail.com>
2024-03-16 12:07:41 +0530

    changes

---
+++ Minimizing_Coins.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+// Macros
+#define ff first
+#define ss second
+#define pb push_back
+#define rep(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define all(x) x.begin(), x.end()
+
+// Typedef
+typedef long long ll;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+    ordered_set; // .find_by_order(), .order_of_key()
+
+// Constants
+const int INF = (int)1e9;
+const int mod = INF + 7;
+/**************************************************************************************/
+
+void runCase(int &testCase) {
+    // cout << "#Case " << testCase << ": ";
+
+    int n, x;
+    cin >> n >> x;
+    vector<int> coins(n);
+    for (auto &e : coins) cin >> e;
+    vector<int> dp(x + 1, INF);
+    dp[0] = 0;
+    for (int i = 1; i <= x; i++) {
+        for (auto &e : coins) {
+            if (i - e >= 0) {
+                dp[i] = min(dp[i], dp[i - e] + 1);
+            }
+        }
+    }
+    if (dp[x] == INF)
+        cout << -1 << "\n";
+    else
+        cout << dp[x] << "\n";
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int tests = 1;
+    // cin >> tests;
+
+    rep(i, 1, tests) runCase(i);
+
+    return 0;
+}
